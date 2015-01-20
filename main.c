1e11ae50c14f2acca2aff2779f6eb698f5392cdb
fuurin <komatsu0624@docomo.ne/jp>
2015-01-20 17:15:24 +0900

    1つの数についてだけのプログラムです。

--- main.c
+++ main.c
@@ -3,15 +3,12 @@
 int main()
 {
 	int i, input;
-	scanf("%d",&input);
+	scanf("%d",&i);
 	
-	for (i = 1; i <= input; ++i)
-	{
 		if(i%3==0 && i%5==0)printf("fizzbuzz\n");
 		else if(i%3==0)printf("fizz\n");
 		else if(i%5==0)printf("buzz\n");
 		else printf("%d\n",i);
-	}
 
 	while(1){}
 }
