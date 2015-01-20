f6ccd45e39bba8d9efa1c2e6186b43dcc2f8ec61
fuurin <komatsu0624@docomo.ne/jp>
2015-01-20 17:06:02 +0900

    fizzbuzz成功！

--- main.c
+++ main.c
@@ -2,11 +2,16 @@
 
 int main()
 {
-	int input;
+	int i, input;
 	scanf("%d",&input);
 	
-	if(input%3==0)printf("fizz\n");
-	else printf("\n");
+	for (i = 1; i <= input; ++i)
+	{
+		if(i%3==0 && i%5==0)printf("fizzbuzz\n");
+		else if(i%3==0)printf("fizz\n");
+		else if(i%5==0)printf("buzz\n");
+		else printf("%d\n",i);
+	}
 
 	while(1){}
 }
